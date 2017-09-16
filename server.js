34c2898c46d5c573295a2133bfc083a24ce73e3e
atronov <atronov@gmail.com>
2017-09-17 00:37:44 +0300

    conf

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
 });
