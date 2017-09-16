593b2102a2ceee6cc2c47c53a887fa23a832b4b0
atronov <atronov@gmail.com>
2017-09-17 01:43:44 +0300

    up

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
-var express = require('express');
-var app = express();
-
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+//  OpenShift sample Node application
+var express = require('express'),
+    app     = express();
 
 app.set('view engine', 'jade');
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!'});
 });
 
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
+
+module.exports = app ;
